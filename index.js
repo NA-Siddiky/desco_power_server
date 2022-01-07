56aa3e915b9f8301cd1ba63fa6b3bce983e4c12c
na-siddiky <siddiky.academic@gmail.com>
2022-01-07 22:36:59 +0600

    get bill

--- index.js
+++ index.js
@@ -1,55 +1,53 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const MongoClient = require('mongodb').MongoClient;
-const ObjectId = require('mongodb').ObjectID;
-require('dotenv').config();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ylija.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri =
+    "mongodb+srv://Soldiers:Soldiers1234@cluster0.ylija.mongodb.net/desco?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
 });
 
-client.connect(err => {
+client.connect((err) => {
+    console.log("Error is:", err);
+    console.log("Database connected Successfully");
 
-    console.log("Error is:", err)
-    console.log("Database connected Successfully")
+    // admin
+    const billCollection = client.db("desco").collection("bill");
 
-    // admin 
-    const adminsCollection = client.db("tech-soldiers").collection("admin");
-
-
-
-
-    app.get("/getReviews", (req, res) => {
-        reviewsCollection.find({}).toArray((err, documents) => {
-            res.send(documents);
+    app.post("/add-billing", (req, res) => {
+        const addBill = req.body;
+        console.log(addBill);
+        billCollection.insertOne(addBill).then((result) => {
+            console.log("inserted count", result);
+            res.send(result.insertedCount > 0);
         });
     });
 
-    app.post("/addReviews", (req, res) => {
-        const field = req.body;
-        reviewsCollection.insertMany(field).then(result => {
-            res.send(result);
-            console.log(result.insertedCount);
-        });
+    app.get("/get-bill/:limit", (req, res) => {
+        const { limit } = req.params;
+        billCollection
+            .find()
+            .limit(limit)
+            .toArray((err, items) => {
+                res.send([...items]);
+            });
     });
-
-
-
-
 });
 
 app.listen(port, () => {
